c9c243044f61ec0f5f4c20aa7e61dc177277250b
Mike <95775960+mikefass@users.noreply.github.com>
2022-03-20 09:48:58 -0500

    Add files via upload
    Update to include all blog files

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#home'], footer a[href='#top']").on(
+    "click",
+    function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        // var hash = this.hash;
+        var hash = $(this).attr("hash");
+
+        // Account for Navbar height
+        if (hash === "#home" || hash === "#top") {
+          var adjustHeight = 350;
+        } else if (hash === "#blog" || hash === "/index.html#blog") {
+          var adjustHeight = 192;
+        } else {
+          var adjustHeight = 157;
+        }
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top - adjustHeight,
+          },
+          900
+          function () {
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          }
+        );
+      } // End if
+    }
+  );
+});
